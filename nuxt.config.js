450bb374458feb4523775e07663a23af3a5ba709
AdigunaSuryo7 <sevensag@gmail.com>
2022-01-11 11:04:10 +0700

    initial back to nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,88 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  target: 'static',
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'Suryo Adiguna',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Portofolio page Suryo Adiguna',
+      },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/programmer.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css',
+      },
+    ],
+    googleAnalytics: {
+      id: 'UA-173143968-2',
+    },
+  },
+
+  server: {
+    port: 3434,
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+    { src: '@/plugins/vue-typed.js' },
+    { src: '@/plugins/vue-screen-size.js', mode: 'client' },
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    ['@nuxtjs/google-analytics', { id: 'UA-173143968-2' }],
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
+
+  // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    treeShake: true,
+    defaultAssets: false,
+    theme: {
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
+}
---
+++ pages/praktikum/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container id="hero" fluid>
+    <v-row
+      class="d-flex align-center justify-center flex-column"
+      style="height: 100%"
+    >
+      <vue-typed-js
+        :strings="['_tender']"
+        :typeSpeed="100"
+        :startDelay="100"
+        :backSpeed="100"
+        :loop="true"
+      >
+        <div class="white--text text-sm-h4 text-h5 text-center">
+          <div>
+            Find M<a
+              class="white--text"
+              style="text-decoration: none; cursor: none"
+              :href="link"
+              >e</a
+            >
+          </div>
+          <span class="typing" style="color: #e31b6d"></span>
+        </div>
+      </vue-typed-js>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      link: 'praktikum/123980',
+    }
+  },
+  mounted() {
+    this.$ga.page(this.$router)
+    // eslint-disable-next-line no-console
+    console.log('bismillah')
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+#hero {
+  height: 100vh !important;
+  width: 100vw !important;
+  background-color: #252934;
+}
+</style>
